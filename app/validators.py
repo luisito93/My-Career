647d1f3f044e280c9c14a6351e913518aa5d9748
chams agouni <chamsagouni8@gmail.com>
2018-08-20 11:46:50 +0100

    Fixing Bugs
    - Fixed Opencv Cascade
    - Fixed Default Image in Header

--- app/validators.py
+++ app/validators.py
@@ -4,6 +4,7 @@ from django.utils.crypto import get_random_string
 from PIL import Image
 import numpy as np
 import cv2
+from django.contrib.staticfiles.storage import staticfiles_storage
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]
     valid_extensions = ['.doc', '.docx', '.pdf', '.rtf', '.txt', '.odt', '.wps']
@@ -21,12 +22,15 @@ def image_path(image, filename):
     return os.path.join(upload_to, filename)
 
 def face_detiction(image):
-    im = Image.open(image)
-    face_cascade = cv2.CascadeClassifier('C:/Users/agoun/Desktop/haarcascade_frontalface_default.xml')
-    image = np.array(im)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    if len(faces) < 1:
-        raise ValidationError(u'Please upload a picture of your face')
-    if len(faces) > 1:
-        raise ValidationError(u'Your Picture have more than 1 face,Please upload a picture of your face')
+    print(image.name)
+    if image.name != "/default.jpg":
+        cascade_path = staticfiles_storage.path('opencv/haarcascade_frontalface_default.xml')
+        im = Image.open(image)
+        face_cascade = cv2.CascadeClassifier(cascade_path)
+        image = np.array(im)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        if len(faces) < 1:
+            raise ValidationError(u'Please upload a picture of your face')
+        if len(faces) > 1:
+            raise ValidationError(u'Your Picture have more than 1 face, Please upload a picture of your face')
--- lazher/settings.py
+++ lazher/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'app',
     'api',
     'django_countries',
-    'mathfilters',
 ]
 
 MIDDLEWARE = [
@@ -80,11 +79,10 @@ WSGI_APPLICATION = 'lazher.wsgi.application'
 
 
 DATABASES = {
-'default':{
- 'ENGINE':'django.db.backends.sqlite3', 'NAME':os.path.join(BASE_DIR,'db.sqlite3')
+    'default':{
+     'ENGINE':'django.db.backends.sqlite3', 'NAME':os.path.join(BASE_DIR,'db.sqlite3')
+    }
 }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -124,6 +122,8 @@ LOGIN_URL = '/login'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'assets'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
