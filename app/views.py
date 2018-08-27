ddf358fb5c976f9ee91f8e317d040f5b7995f0ff
chams agouni <chamsagouni8@gmail.com>
2018-08-27 22:52:11 +0100

    Change Style Colors

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from jobs.models import Jobs
 
 def index(request):
+    jobs = Jobs.objects.all().order_by("-created")
     context = {
         'title': 'Find a Job | OdamaCareer',
+        'jobs':jobs,
     }
 
     return render(request,"index.html",context)
