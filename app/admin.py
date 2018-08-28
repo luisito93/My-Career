d11c513b51eb71d52b0a8951cf2590d28a93fa60
Sunil <sunil9766@gmail.com>
2018-08-28 18:45:33 +0530

    fix: flake8 fixes

--- app/admin.py
+++ app/admin.py
@@ -1,28 +1,41 @@
 from django.contrib import admin
 from company.models import Company, Office
-from userprofile.models import Cv, UserProfile,Education, School, Experience, Award
+from userprofile.models import (
+    Cv,
+    UserProfile,
+    Education,
+    School,
+    Experience,
+    Award
+)
 
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'city','since', 'contact_info')
+    list_display = ('title', 'city', 'since', 'contact_info')
+
 
 class CvAdmin(admin.ModelAdmin):
-    list_display = ('user', 'job_title','cv') 
+    list_display = ('user', 'job_title', 'cv')
+
 
 class User_profileAdmin(admin.ModelAdmin):
-    list_display = ('user','slug', 'city', 'country','gender','created')
-    
+    list_display = ('user', 'slug', 'city', 'country', 'gender', 'created')
+
+
 class EducationAdmin(admin.ModelAdmin):
-    list_display = ('education_level', 'degree','year_from','year_to')
+    list_display = ('education_level', 'degree', 'year_from', 'year_to')
+
 
 class ExperienceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company', 'job_title','job_from','job_to')
+    list_display = ('user', 'company', 'job_title', 'job_from', 'job_to')
+
 
 class AwardAdmin(admin.ModelAdmin):
     list_display = ('user', 'company', 'year')
 
+
 admin.site.register(Company, CompanyAdmin)
-admin.site.register(Cv ,CvAdmin)
+admin.site.register(Cv, CvAdmin)
 admin.site.register(UserProfile, User_profileAdmin)
 admin.site.register(Education, EducationAdmin)
 admin.site.register(School)
